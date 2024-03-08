3de64c8675e9c1c45e7c069b712280481c38deda
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-08 19:08:26 +0000

    Pushing 5 to rename my repo...

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,40 @@
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_not_fun(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    try:
+        return f"{int(n)} is a number"
+    except ValueError:
+        abort(404)
+
+@app.route("/number_template/<n>", strict_slashes=False)
+def number_html(n):
+    try:
+        num = int(n)
+        return render_template('5-number.html', content=num)
+    except ValueError:
+        abort(404)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
