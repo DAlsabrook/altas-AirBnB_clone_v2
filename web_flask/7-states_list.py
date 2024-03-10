7bdaaaa42a10d6f7c777f551d15c55169c1a560c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-10 03:39:17 +0000

    Whoa it actually makes the html for you. cool.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is a module to start displaying things to the front end
+"""
+from flask import Flask, render_template, abort
+from models.__init__ import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(error):
+    storage.close()
+
+
+@app.route("/states_list")
+def states_list():
+    try:
+        states_dict = storage.all(State)
+        sorted_dict = {k: v for k, v in sorted(states_dict.items(),
+                                               key=lambda item: item[1].name)}
+        return render_template('7-states_list.html', states_list=sorted_dict)
+    except ValueError:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
