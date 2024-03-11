b690c1dc5e524c38bf683bdf50055be354e15ace
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-03-11 17:37:21 +0000

    First attempt at 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -5,18 +5,19 @@ This is a module to start displaying things to the front end
 from flask import Flask, render_template
 from models.__init__ import storage
 from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
-@app.route("/states")
-@app.route("/states/<id>")
+@app.route("/hbnb_filters")
 def states_id_route(id=None):
     """
     Displays an HTML formatted of cities with a given State id
     """
     states = storage.all(State)
-    return render_template("9-states.html", state_list=states)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", states_dict=states, amenities_dict=amenities)
 
 
 @app.teardown_appcontext
